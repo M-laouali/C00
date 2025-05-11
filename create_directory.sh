375b2f10fe83815b9edcdf2a046d1332d5bfbf7d
M-laouali <mlaoualialmou@gmail.com>
2025-05-11 09:55:35 +0100

    create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-# Créer le répertoir
+# Nom du dossier à créer
+DIR_NAME="test_directory"
+
+# Création du dossier
+mkdir "$DIR_NAME"
+
+# Message de confirmation
+echo "Le répertoire '$DIR_NAME' a été créé."
