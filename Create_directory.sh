1e493f76e6f88adf8b7ecbd37fd624573247a568
M-laouali <mlaoualialmou@gmail.com>
2025-05-11 06:45:52 +0100

    Create test_directory.sh

--- Create_directory.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Créer le répertoire
-mkdir test_directory
-
-# Message de confirmation
-echo "Le répertoire 'test_directory' a été créé."
---
+++ test_directory.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Créer le répertoire
+mkdir test_directory
