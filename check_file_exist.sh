34672a6ac98cc0207896b7d9ae8cc9b1ac2128dd
M-laouali <mlaoualialmou@gmail.com>
2025-05-11 11:45:05 +0200

    Add files via upload

---
+++ check_file_exist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Demander à l'utilisateur de saisir le nom du fichier
+echo "Entrez le nom du fichier à vérifier :"
+read filename
+
+# Vérifier si le fichier existe
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
+fi
