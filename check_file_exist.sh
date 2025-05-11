d75cbffe2eb268f1e421244279d066abcc5388e4
M-laouali <mlaoualialmou@gmail.com>
2025-05-11 12:13:20 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Demande à l'utilisateur de saisir le nom du fichier
-echo -n "Entrez le nom du fichier :"
+# Demande le nom du fichier à l'utilisateur
+read -p "Entrez le nom du fichier: "filename
 
-# Vérifie si le fichier existe
-if [ -f "$filename" ]; then
-    echo "Le fichier '$filename' existe."
+# Vérifie si le fichier existe et est un fichier régulier
+if [ -f "filename" ]; then
+    echo "Le fichier 'filename' existe."
 else
-    echo "Le fichier '$filename' n'existe pas."
+    echo "Le fichier 'filename' n'existe pas."
 fi
