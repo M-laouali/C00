4897584c6da9fd14c4975d57fd6f012e381271a9
M-laouali <mlaoualialmou@gmail.com>
2025-05-11 09:35:37 +0100

    Create hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-  echo ''Hello, World !''
-#c'est comme si je demande à la machine de m'afficher du texte 
+  echo ''Hello, World!''
+#c'est comme si je demande à la machine de m'afficher du texte
