b1b10aaca0256fff7cc916490cc40eafe3487a16
M-laouali <mlaoualialmou@gmail.com>
2025-05-11 11:07:57 +0100

    Check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Demande du nom du fichier ou dossier
-read -p "Entrez le nom du fichier ou dossier : " name
-
-# Vérifie le type
-if [ -f "$name" ]; then
-    echo "'$name' est un fichier."
-elif [ -d "$name" ]; then
-    echo "'$name' est un dossier."
-else
-    echo "'$name' n'existe pas."
-fi
+[ -f nom_du_fichier ]
