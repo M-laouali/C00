1b011f232308da2a84e57cd1816c413d4fd522c2
M-laouali <mlaoualialmou@gmail.com>
2025-05-15 17:23:26 +0100

    Update list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Script pour afficher la liste de tous les fichiers du répertoire courant
-# Objectif : comprendre la commande ls et ses options
+# Afficher la liste de tous les fichiers du répertoire courant
 
 echo "Liste de tous les fichiers et dossiers dans le répertoire courant :"
-ls -l    # Affiche les fichiers avec détails (droits, taille, date, etc.)
 
-echo ""
 echo "Liste uniquement des fichiers (sans les dossiers) :"
 ls -p | grep -v /    # -p ajoute un / aux dossiers, grep -v / filtre les dossiers
