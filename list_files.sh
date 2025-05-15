c46db2a37bd89001fbbeae0a945da00605bedeb0
M-laouali <mlaoualialmou@gmail.com>
2025-05-15 23:00:29 +0100

    Update list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -2,5 +2,5 @@
 
 # Afficher la liste de tous les fichiers du répertoire courant
 
-echo "Liste uniquement des fichiers (sans les dossiers) :"
+echo "Liste de tout les fichiers du répertoire courant :"
 ls -p | grep -v /    # -p ajoute un / aux dossiers, grep -v / filtre les dossiers
