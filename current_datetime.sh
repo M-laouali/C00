8367bb38108ab185332a1c76a67aef8f1b6b0c41
M-laouali <mlaoualialmou@gmail.com>
2025-05-15 17:18:15 +0100

    Update current_datetime.sh

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Récupère chaque partie de la date/heure
-year=$(date +%Y)
-month=$(date +%m)
-day=$(date +%d)
-hour=$(date +%H)
-minute=$(date +%M)
-second=$(date +%S)
-
-# Affiche le format désiré
-printf "%s-%s-%s %s:%s:%s\n" "$year" "$month" "$day" "$hour" "$minute" "$second"
+# date permets d'afficher la date selon le format personnalisé ci après 
+date "+%Y-%m-%d %H:%M:%S"
